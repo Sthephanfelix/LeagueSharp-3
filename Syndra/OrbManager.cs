dfc95ee31ca491e8b916c9862d834062a3637543
Sthephanfelix <elkorean@dkdk.clsco>
2015-05-16 15:01:58 +0900

    Update OrbManager.cs

--- Syndra/OrbManager.cs
+++ Syndra/OrbManager.cs
@@ -67,7 +67,7 @@ namespace Syndra
 
         private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
         {
-            if (args.PacketData[0] == 0xBA)
+            if (args.PacketData[0] == 0x24)
             {
                 var packet = new GamePacket(args.PacketData);
                 packet.Position = 2;
